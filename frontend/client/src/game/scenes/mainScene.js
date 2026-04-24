91162407f4c7593598cb7ac50ab516e1db014a27
MsalC1 <msalmeron2005@gmail.com>
2026-04-23 22:49:49 -0700

    deleted some shit, added a few more shit

--- frontend/client/src/game/scenes/mainScene.js
+++ frontend/client/src/game/scenes/mainScene.js
@@ -4,15 +4,19 @@ export default class MainScene extends Phaser.Scene  {
     player;
 
     preload(){
-        this.load.image("player", "src/assets/hero.png");
+        const player_sprite = "src/assets/PlayerCharacter/spritesheets/stand-facing-right-51p-x-512px.png";
+        this.load.image("player", player_sprite);
     }
 
     create(){
         this.player = this.add.sprite(1, 1, "player");
+        this.player.setDisplaySize(64, 64);
         this.physics.add.existing(this.player);
-        this.player.body.setVelocity(150, 150);
+        // this.player.body.setVelocity(150, 150);
+        this.player.body.setCollideWorldBounds(true, 1, 1);
     }
 
     update() {
+        this.player.x = this.input.x;
     }
 }
