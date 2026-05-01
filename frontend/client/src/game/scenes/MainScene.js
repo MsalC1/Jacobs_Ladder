70ec1b1f237e948842cc59b5989f1c48a6400249
VladyslavRutskyi <98677290+VladyslavRutskyi@users.noreply.github.com>
2026-05-01 01:08:05 +0000

    left, right + bounds

--- frontend/client/src/game/scenes/MainScene.js
+++ frontend/client/src/game/scenes/MainScene.js
@@ -8,11 +8,11 @@ import JumpMeter from "../systems/JumpMeter";
 export default class MainScene extends Phaser.Scene  {
     preload(){
         const playerRight   = new URL("../../assets/PlayerCharacter/spritesheets/Character_WalkingRight.png", import.meta.url).href;
-        const playerLeft    = new URL("../../assets/PlayerCharacter/spritesheets/Character_WalkingLeft.png", import.meta.url).href;
+        const playerLeft    = new URL("../../assets/PlayerCharacter/spritesheets/Character_WalkingLeft.PNG", import.meta.url).href;
 
         this.load.image('background', 'assets/Locations/HELL.png');
-        this.load.spritesheet('player-right', playerRight, { frameWidth: 512, frameHeight: 512 });
-        this.load.spritesheet('player-left', playerLeft, { frameWidth: 512, frameHeight: 512 });
+        this.load.spritesheet('player-right', playerRight, { frameWidth: 185, frameHeight: 240 });
+        this.load.spritesheet('player-left', playerLeft, { frameWidth: 180, frameHeight: 240 });
     }
 
     create(){
@@ -34,14 +34,14 @@ export default class MainScene extends Phaser.Scene  {
     createPlayerAnimations() {
         this.anims.create({
             key: 'walk-right',
-            frames: this.anims.generateFrameNumbers('player-right', { start: 0, end: 3 }),
-            frameRate: 10,
+            frames: this.anims.generateFrameNumbers('player-right', { start: 0, end: 9 }),
+            frameRate: 13,
             repeat: -1
         });
         this.anims.create({
             key: 'walk-left',
-            frames: this.anims.generateFrameNumbers('player-left', { start: 0, end: 3 }),
-            frameRate: 10,
+            frames: this.anims.generateFrameNumbers('player-left', { start: 0, end: 9 }),
+            frameRate: 13,
             repeat: -1
         });
         this.anims.create({
