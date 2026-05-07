b406d184ae48d78ef6932598f7f39601644cca87
Josue Chavez <josuechavez1022@gmail.com>
2026-05-06 23:40:45 -0700

    added buttons for log in and sign up

--- frontend/client/src/pages/LoginPage.jsx
+++ frontend/client/src/pages/LoginPage.jsx
@@ -3,9 +3,12 @@ import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
     const [nickname, setNickname] = useState("");
+    const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
     function handleSubmit(e) {
+        // we need to do HTTP methods here!
+        
         e.preventDefault();
 
         if (!nickname.trim()) return;
@@ -22,13 +25,22 @@ function LoginPage() {
             <form onSubmit={handleSubmit} style={styles.form}>
                 <input
                 type="text"
-                placeholder="Enter nickname"
+                placeholder="Enter username"
                 value={nickname}
                 onChange={(e) => setNickname(e.target.value)}
                 style={styles.input}
                 />
 
-                <button type="submit" style={styles.button}>Enter Lobby</button>
+                <input
+                type="text"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                style={styles.input}
+                />
+
+                <button type="submit" style={styles.button}>Log In</button>
+                <button type="submit" style={styles.button}>Sign Up</button>
             </form>
         </div>
     );
